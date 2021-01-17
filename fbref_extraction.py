2ef961a47d07c2fed3c776ec138af1517d12d325
ysho <ysho2@illinois.edu>
2021-01-17 09:20:08 -0600

    Resolved sorting issues and missing data points

--- fbref_extraction.py
+++ fbref_extraction.py
@@ -30,6 +30,9 @@ def extract_goalkeeper_stats(player_link):
 
     try:
         df.drop(df[df["Pos"] == "On matchday squad, but did not play"].index, inplace=True)
+        df['sort'] = df['Round'].str.extract('(\d+)', expand=False).astype(int)
+        df.sort_values('sort',inplace=True)
+        df = df.drop('sort', axis=1)
         f = open("goalkeeperstats_epl.csv")
         df.to_csv('goalkeeperstats_epl.csv', index=False, header=False, mode = 'a')
         f.close()
@@ -50,6 +53,9 @@ def extract_goalkeeper_stats(player_link):
         df["Number of defensive actions outside of penalty area"] = np.nan
         df["Average distance from goal to perform defensive actions"] = np.nan
         df.drop(df[df["Pos"] == "On matchday squad, but did not play"].index, inplace=True)
+        df['sort'] = df['Round'].str.extract('(\d+)', expand=False).astype(int)
+        df.sort_values('sort',inplace=True)
+        df = df.drop('sort', axis=1)
         df.to_csv('goalkeeperstats_epl.csv', index=False)
     
 def extract_player_stats(player_link):
@@ -170,11 +176,17 @@ def extract_player_stats(player_link):
     concatenated.drop(concatenated[concatenated["Date"] == "Date"].index, inplace=True)
     try:
         concatenated.drop(concatenated[concatenated["Pos"] == "On matchday squad, but did not play"].index, inplace=True)
+        concatenated['sort'] = concatenated['Round'].str.extract('(\d+)', expand=False).astype(int)
+        concatenated.sort_values('sort',inplace=True)
+        concatenated = concatenated.drop('sort', axis=1)
         f = open("playerstats_epl.csv")
         concatenated.to_csv('playerstats_epl.csv', index=False, header=False, mode = 'a')
         f.close()
     except:
         concatenated.drop(concatenated[concatenated["Pos"] == "On matchday squad, but did not play"].index, inplace=True)
+        concatenated['sort'] = concatenated['Round'].str.extract('(\d+)', expand=False).astype(int)
+        concatenated.sort_values('sort',inplace=True)
+        concatenated = concatenated.drop('sort', axis=1)
         concatenated.to_csv('playerstats_epl.csv', index=False)
 
 def extractName(player_link):
@@ -197,7 +209,7 @@ soup = BeautifulSoup(html_page, 'html.parser')
 text_contains_squads = "/en/squads/"
 teams_array = []
 final_team_array = []
-teams_exclude = ['/en/squads/', '/en/squads/822bd0ba/Liverpool-Stats', '/en/squads/411b1108/Arsenal-Women-Stats', '/en/squads/206d90db/Barcelona-Stats', '/en/squads/e2d8892c/Paris-Saint-Germain-Stats', '/en/squads/7f2012ad/Lyon-Women-Stats', '/en/squads/054efa67/Bayern-Munich-Stats', '/en/squads/a1393014/Wolfsburg-Women-Stats', '/en/squads/e0652b02/Juventus-Stats', '/en/squads/613577b8/Juventus-Women-Stats', '/en/squads/795ca75e/Boca-Juniors-Stats', '/en/squads/2466c132/Sydney-FC-Stats', '/en/squads/6082332e/Melbourne-City-Women-Stats', '/en/squads/50f2a074/Red-Bull-Salzburg-Stats', '/en/squads/f1e6c5f1/Club-Brugge-Stats', '/en/squads/e69cb5b6/Bolivar-Stats', '/en/squads/639950ae/Flamengo-Stats', '/en/squads/488c6ba1/Ludogorets-Razgrad-Stats', '/en/squads/8514c671/Forge-FC-Stats', '/en/squads/3e3fbf36/Univ-Catolica-Stats', '/en/squads/1837b9f6/Guangzhou-Evergrande-Taobao-Stats', '/en/squads/e0b973a6/Atletico-Nacional-Stats', '/en/squads/edd0d381/Dinamo-Zagreb-Stats', '/en/squads/111cbfb1/Slavia-Prague-Stats', '/en/squads/3c4fb635/Midtjylland-Stats', '/en/squads/8c71aef1/Barcelona-SC-Stats', '/en/squads/d7319d80/HJK-Helsinki-Stats', '/en/squads/2fdb4aef/Olympiacos-Stats', '/en/squads/6611f992/Ferencvaros-Stats', '/en/squads/3249478a/Goa-Stats', '/en/squads/95f42e44/Persepolis-Stats', '/en/squads/858d58b2/Kawasaki-Frontale-Stats', '/en/squads/ae23a242/Jeonbuk-Stats', '/en/squads/972e2539/Al-Hilal-Stats', '/en/squads/18d3c3a3/America-Stats', '/en/squads/19c3f8c4/Ajax-Stats', '/en/squads/9522e7b4/PSV-Stats', '/en/squads/174bd5a0/Molde-Stats', '/en/squads/ca6492f2/LSK-Kvinner-Stats', '/en/squads/4d4fc0b8/Olimpia-Asuncion-Stats', '/en/squads/8917b8a9/Sporting-Cristal-Stats', '/en/squads/a73408a7/Legia-Warsaw-Stats', '/en/squads/5e876ee6/Porto-Stats', '/en/squads/ff04e205/CFR-Cluj-Stats', '/en/squads/97d80fef/Mamelodi-Sundowns-Stats', '/en/squads/98ce363d/Zenit-Stats', '/en/squads/b81aa4fa/Celtic-Stats', '/en/squads/099c6eb5/Red-Star-Belgrade-Stats', '/en/squads/4b682260/Young-Boys-Stats', '/en/squads/f3d8c8b9/Malmo-Stats', '/en/squads/89f584e1/KopparbergsGoteborg-Stats', '/en/squads/0f9294bd/Besiktas-Stats', '/en/squads/e89d5a28/Shakhtar-Donetsk-Stats', '/en/squads/26ebba72/Nacional-Stats', '/en/squads/6218ebd4/Seattle-Sounders-FC-Stats', '/en/squads/85c458aa/North-Carolina-Courage-Stats', '/en/squads/2583cf18/Caracas-Stats']
+teams_exclude = ['/en/squads/', '/en/squads/411b1108/Arsenal-Women-Stats', '/en/squads/206d90db/Barcelona-Stats', '/en/squads/e2d8892c/Paris-Saint-Germain-Stats', '/en/squads/7f2012ad/Lyon-Women-Stats', '/en/squads/054efa67/Bayern-Munich-Stats', '/en/squads/a1393014/Wolfsburg-Women-Stats', '/en/squads/e0652b02/Juventus-Stats', '/en/squads/613577b8/Juventus-Women-Stats', '/en/squads/795ca75e/Boca-Juniors-Stats', '/en/squads/2466c132/Sydney-FC-Stats', '/en/squads/6082332e/Melbourne-City-Women-Stats', '/en/squads/50f2a074/Red-Bull-Salzburg-Stats', '/en/squads/f1e6c5f1/Club-Brugge-Stats', '/en/squads/e69cb5b6/Bolivar-Stats', '/en/squads/639950ae/Flamengo-Stats', '/en/squads/488c6ba1/Ludogorets-Razgrad-Stats', '/en/squads/8514c671/Forge-FC-Stats', '/en/squads/3e3fbf36/Univ-Catolica-Stats', '/en/squads/1837b9f6/Guangzhou-Evergrande-Taobao-Stats', '/en/squads/e0b973a6/Atletico-Nacional-Stats', '/en/squads/edd0d381/Dinamo-Zagreb-Stats', '/en/squads/111cbfb1/Slavia-Prague-Stats', '/en/squads/3c4fb635/Midtjylland-Stats', '/en/squads/8c71aef1/Barcelona-SC-Stats', '/en/squads/d7319d80/HJK-Helsinki-Stats', '/en/squads/2fdb4aef/Olympiacos-Stats', '/en/squads/6611f992/Ferencvaros-Stats', '/en/squads/3249478a/Goa-Stats', '/en/squads/95f42e44/Persepolis-Stats', '/en/squads/858d58b2/Kawasaki-Frontale-Stats', '/en/squads/ae23a242/Jeonbuk-Stats', '/en/squads/972e2539/Al-Hilal-Stats', '/en/squads/18d3c3a3/America-Stats', '/en/squads/19c3f8c4/Ajax-Stats', '/en/squads/9522e7b4/PSV-Stats', '/en/squads/174bd5a0/Molde-Stats', '/en/squads/ca6492f2/LSK-Kvinner-Stats', '/en/squads/4d4fc0b8/Olimpia-Asuncion-Stats', '/en/squads/8917b8a9/Sporting-Cristal-Stats', '/en/squads/a73408a7/Legia-Warsaw-Stats', '/en/squads/5e876ee6/Porto-Stats', '/en/squads/ff04e205/CFR-Cluj-Stats', '/en/squads/97d80fef/Mamelodi-Sundowns-Stats', '/en/squads/98ce363d/Zenit-Stats', '/en/squads/b81aa4fa/Celtic-Stats', '/en/squads/099c6eb5/Red-Star-Belgrade-Stats', '/en/squads/4b682260/Young-Boys-Stats', '/en/squads/f3d8c8b9/Malmo-Stats', '/en/squads/89f584e1/KopparbergsGoteborg-Stats', '/en/squads/0f9294bd/Besiktas-Stats', '/en/squads/e89d5a28/Shakhtar-Donetsk-Stats', '/en/squads/26ebba72/Nacional-Stats', '/en/squads/6218ebd4/Seattle-Sounders-FC-Stats', '/en/squads/85c458aa/North-Carolina-Courage-Stats', '/en/squads/2583cf18/Caracas-Stats']
 text = soup.find_all("a", href=True)
 for a in text:
     if text_contains_squads in a['href']:
@@ -208,7 +220,7 @@ for i in teams_array:
     final_team_array.append("https://fbref.com"+i)
 
 final_team_array = list(set(final_team_array))
-
+print(final_team_array)
 # Get List of all Premier League Players and their respective links
 player_array = []
 player_final_array = []
@@ -216,7 +228,7 @@ final_link = []
 
 text_contains_players = "/en/players/"
 
-players_to_remove = ['/en/players/', '/en/players/5b97f5ec/Yuki-Nagasato', '/en/players/b5b0068e/Onome-Ebi', '/en/players/e2e9c250/Santi-Cazorla', '/en/players/293211e1/Aritz-Aduriz', '/en/players/b418dbd4/Raul-Garcia', '/en/players/71c5f16f/Andrea-Pirlo', '/en/players/cda50d6b/Mana-Iwabuchi', '/en/players/f07be45a/Wayne-Rooney', '/en/players/82dbf623/Fernando-Llorente', '/en/players/ee65d412/Precious-Dede', '/en/players/e342ad68/Mohamed-Salah', '/en/players/e358587b/Pepe-Reina', '/en/players/655e7bd0/Alexandra-Popp', '/en/players/63b0ca6b/Alvaro-Negredo', '/en/players/59ef6268/Tobin-Heath']
+players_to_remove = ['/en/players/', '/en/players/5b97f5ec/Yuki-Nagasato', '/en/players/b5b0068e/Onome-Ebi', '/en/players/e2e9c250/Santi-Cazorla', '/en/players/293211e1/Aritz-Aduriz', '/en/players/b418dbd4/Raul-Garcia', '/en/players/71c5f16f/Andrea-Pirlo', '/en/players/cda50d6b/Mana-Iwabuchi', '/en/players/f07be45a/Wayne-Rooney', '/en/players/82dbf623/Fernando-Llorente', '/en/players/ee65d412/Precious-Dede', '/en/players/e358587b/Pepe-Reina', '/en/players/655e7bd0/Alexandra-Popp', '/en/players/63b0ca6b/Alvaro-Negredo', '/en/players/59ef6268/Tobin-Heath']
 
 
 for team_url in final_team_array:
@@ -231,7 +243,7 @@ for team_url in final_team_array:
             if a["href"] not in players_to_remove:
                 player_array.append(a["href"])
     player_array = list(set(player_array))
-
+print(player_array)
 for i in player_array:
     if "matchlog" in i:
         player_final_array.append("https://fbref.com"+i)
